8109194ac14cb9d5fccd154cf17e68ce0307011e
wiren911 <31924528+wiren911@users.noreply.github.com>
2017-10-23 22:06:18 +0200

    Add files via upload

---
+++ Validator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator
+{
+     class Program
+    {
+        //public string title { get; set; }
+        //public string description { get; set; }
+        //public string link { get; set; }
+
+        //public class podInfo
+        //{
+        //    public List<Program> info = new List<Program>();
+
+        //    public void Add(Program podavsnitt) => info.Add(podavsnitt);
+        //}
+         
+    }
+}
