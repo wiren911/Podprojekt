4aef7c8452c8eda4e243f48ae0bf6fc4fa96027d
wiren911 <31924528+wiren911@users.noreply.github.com>
2017-10-23 22:05:33 +0200

    Add files via upload

---
+++ DataHandler/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHandler
+{
+        public class Podcast
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+            public List<Episode> Episodes { get; set; }
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        public class Episode
+        {
+            public string Name { get; set; }
+            public string Mp3Path { get; set; }
+        }
+
+        public class Category
+        {
+            public string Name { get; set; }
+            public string Path { get; set; }
+            public List<Podcast> Podcasts { get; set; }
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+    }
+
---
+++ DataHandler/Class2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataHandler
+{
+    public class Xmlhandler
+    {
+
+        public void SaveFeed(string name, string categoryName, string url)
+        {
+            //var path = Directory.GetCurrentDirectory() + @"\category\";
+            //Directory.CreateDirectory(path);
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\categories\" + categoryName);
+            var path = Directory.GetCurrentDirectory() + @"\categories\" + categoryName + @"\" + name + ".xml";
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(url, path);
+                client.Dispose();
+            }
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            var path = Directory.GetCurrentDirectory() + @"\categories\";
+            var categories = Directory.GetDirectories(path);
+            List<Category> categoriesInFolder = new List<Category>();
+            foreach (var category in categories)
+            {
+                var dirInfo = new DirectoryInfo(category);
+                var name = dirInfo.Name;
+                categoriesInFolder.Add(new Category() { Name = name, Path = category });
+            }
+            return categoriesInFolder;
+        }
+
+        public List<Podcast> GetAllFeedsInCategory(string category)
+        {
+            var path = Directory.GetCurrentDirectory() + @"\categories\" + category;
+            var podcasts = Directory.GetFiles(path);
+            List<Podcast> pods = new List<Podcast>();
+            foreach (var podcast in podcasts)
+            {
+                var file = Path.GetFileName(podcast);
+                pods.Add(new Podcast() { Name = file, Url = podcast });
+            }
+            return pods;
+        }
+    }
+}
